e9d901a3272cd565516f1ffd8a8b60476bc2d075
Deep <45520903+deepmeena70@users.noreply.github.com>
2023-02-17 15:52:03 +0530

    optimized

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 (() => {
   console.log('js is running');
 
-  const glass_container = document.querySelector('.glass-container');
-  const btn = document.querySelectorAll('.btn');
-  const prev = document.querySelector('.prev');
-  const next = document.querySelector('.next');
-  const slides = document.querySelectorAll('.slide');
+  const glass_container = document.querySelector('.glass-container'); //get glass container
+  const btn = document.querySelectorAll('.btn'); //get prev and next buttons
+  const prev = document.querySelector('.prev'); //get prev button only
+  const next = document.querySelector('.next'); //get next button only
+  const slides = document.querySelectorAll('.slide'); //get all slides
 
-  const firstIndex = 0;
-  const lastIndex = slides.length - 1;
+  const firstIndex = 0; //as we have array of slides this is our first index
+  const lastIndex = slides.length - 1; //and this is last
 
-  let activeSlide = 0;
+  let activeSlide = 0; //the active slide
 
   const slideInActive = () => {
     slides.forEach((slide) => {
@@ -18,7 +18,8 @@
     });
   };
 
-  prevSlide = () => {
+  //change slide to previous slide
+  const prevSlide = () => {
     console.log('prev');
     slideInActive();
     activeSlide--;
@@ -26,7 +27,8 @@
     slides[activeSlide].classList.add('active');
   };
 
-  nextSlide = () => {
+  //change slide to next slide
+  const nextSlide = () => {
     console.log('next');
     slideInActive();
     activeSlide++;
@@ -34,22 +36,27 @@
     slides[activeSlide].classList.add('active');
   };
 
-  const onMouseEnter = (e) => {
+  //work on mouse enter
+  const onMouseEnter = () => {
     console.log('onMouseEnter');
+    //change the button width
     btn.forEach((button) => {
       button.style.width = '12vw';
     });
+    //trigger on click
     prev.addEventListener('click', prevSlide);
     next.addEventListener('click', nextSlide);
   };
 
-  const onMouseLeave = (e) => {
+  //reset on mouseleave
+  const onMouseLeave = () => {
     console.log('onMouseLeave');
     btn.forEach((button) => {
       button.style.width = '95vw';
     });
   };
 
+  //attach eventlisteners to glass container
   glass_container.addEventListener('mouseenter', onMouseEnter);
   glass_container.addEventListener('mouseleave', onMouseLeave);
 })();
